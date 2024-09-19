f57d687fb6d80113f8d184ce6ec9917c0b1aab53
Rony Hossain <mdrony.ain@gmail.com>
2024-09-19 15:56:19 +0600

    commit add

--- src/components/frontend/body/home/VegetableProducts.tsx
+++ src/components/frontend/body/home/VegetableProducts.tsx
@@ -6,10 +6,10 @@ const VegetableProducts = () => {
     <div className="flex flex-col gap-8 mx-4 sm:mx-8 md:mx-16 lg:mx-32 py-10">
       <h1 className="text-4xl font-semibold text-slate-700 mb-3">Fresh Organic Vegetables</h1>
       <div className="flex gap-6">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard signal={false} />
+        <ProductCard signal={false} />
+        <ProductCard signal={false} />
+        <ProductCard signal={false} />
       </div>
     </div>
   )
